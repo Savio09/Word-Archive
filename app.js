65a382f3f281d6aea0a5fde8a35d006906722a44
Savio09 <44920970+Savio09@users.noreply.github.com>
2023-02-07 00:14:05 +0100

    update ui

--- app.js
+++ app.js
@@ -1,119 +1,69 @@
-    let search = document.querySelector('#search')
-    let searchBtn = document.querySelector('#search-btn')
-    let font = document.querySelector('select')
-    let body = document.querySelector('body')
-    let play = document.querySelector('#play')
-   
-    
-    
-    font.onchange = () => {
+let search = document.querySelector('#search'),
+    searchBtn = document.querySelector('#search-btn'),
+    font = document.querySelector('select'),
+    body = document.querySelector('body'),
+    toggle = document.querySelector('.toggle');
+
+toggle.addEventListener('click', () => {
+    body.classList.toggle("dark-mode")
+})
+
+font.onchange = () => {
     console.log(font.value)
     body.style.fontFamily = font.value;
-    }
+}
 
-    searchBtn.addEventListener('click', query)
+searchBtn.addEventListener('click', query)
 
-    function query(e) {
+function query(e) {
     e.preventDefault();
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${search.value}`)
-    .then(res => res.json())
-    .then(data => {
-    let output = `
-    <div class= "result">
-        <div>
-            <h1 class="title">${search.value}</h1>
-            <span class="transcript">${data[0].phonetic}</span>
-        </div>
-        <button class="play" id="play"><i class="fa-solid fa-play"></i></button>
-    </div>
-    `
-    //let audio= new Audio("https:"+ data[0].phonetics[0].audio)
-    play.onclick = () => {
-    alert(data[0].phonetics[0].audio)
-    //audio.play();
-    }
-    data.forEach((uniqueRes, index) => {
-        let meaning = uniqueRes.meanings;
-        meaning.forEach((def, index) => {
+        .then(res => res.json())
+        .then(data => {
+            let transcription = data[0].phonetic || data[0].phonetics[1].text
+            let output = `
+            <div class= "result">
+                <div>
+                    <h1 class="title">${search.value}</h1>
+                    <span class="transcript">${transcription}</span>
+                </div>
+                <button class="play" id="play"><i class="fa-solid fa-play"></i></button>
+            </div>
+            `
+            data.forEach((uniqueRes, index) => {
+                let meaning = uniqueRes.meanings;
+                console.log(meaning)
 
-            if (def.partOfSpeech === 'noun') {
-                const nouns = [...def.definitions]
-                if (nouns.length > 1) {
-                    output += `<p>${def.partOfSpeech}</p>`
-                }
-                nouns.forEach(noun => {
-                    output += `<li>${noun.definition}</li>`
+                meaning.forEach((def, index) => {
+
+                    if (def.partOfSpeech === 'noun') {
+                        output += `<h3>${def.partOfSpeech}</h3>`
+                        const nouns = [...def.definitions]
+                        const nounsList = nouns.map(noun => noun)
+                        const nounMeanings = nounsList.map(x => x.definition)
+                        nounMeanings.forEach(meaning => output +=
+                            `<li class='definition'>${meaning}</li>`)
+                    }
+
+                    if (def.partOfSpeech === 'verb') {
+                        output += `<h3>${def.partOfSpeech}</h3>`
+                        const verbs = [...def.definitions]
+                        const verbsList = verbs.map(verb => verb)
+                        const verbMeanings = verbsList.map(x => x.definition)
+                        verbMeanings.forEach(meaning => output +=
+                            `<li class ='definition'>${meaning}</li>`)
+                    }
                 })
-            }
-            if (def.partOfSpeech === 'verb') {
-                const verb = [...def.definitions]
-                if (verb.length > 1) {
-                    output += `<p>${def.partOfSpeech}</p>`
-                }
-                verb.forEach(verb => {
-                    output += `<li>${verb.definition}</li>`
-                })
-            }
-            if (def.partOfSpeech === 'interjection') {
-                const interjection = [...def.definitions]
-                if (interjection.length > 1) {
-                    output += `<p>${def.partOfSpeech}</p>`
-                }
-                interjection.forEach(interjection => {
-                    output += `<li>${interjection.definition}</li>`
-                })
-            }
-            if (def.partOfSpeech === 'preposition') {
-                const preposition = [...def.definitions]
-                if (preposition.length > 1) {
-                    output += `<p>${def.partOfSpeech}</p>`
-                }
-                preposition.forEach(preposition => {
-                    output += `<li>${preposition.definition}</li>`
-                })
-            }
-            if (def.partOfSpeech === 'adjective') {
-                const adjective = [...def.definitions]
-                if (adjective.length > 1) {
-                    output += `<p>${def.partOfSpeech}</p>`
-                }
-                adjective.forEach(adjective => {
-                    output += `<li>${adjective.definition}</li>`
-                })
-            }
-            if (def.partOfSpeech === 'pronoun') {
-                const pronouns = [...def.definitions]
-                if (pronouns.length >= 1) {
-                    output += `<p>${def.partOfSpeech}</p>`
-                }
-                pronouns.forEach(pronoun => {
-                    output += `<li>${noun.definition}</li>`
-                })
-            }
-            if (def.partOfSpeech === 'adverb') {
-                const adverb = [...def.definitions]
-                if (adverb.length >= 1) {
-                    output += `<p>${def.partOfSpeech}</p>`
-                }
-                pronouns.forEach(adverb => {
-                    output += `<li>${adverb.definition}</li>`
-                })
-            }
-            if (def.partOfSpeech === 'conjunction') {
-                const conjunction = [...def.definitions]
-                if (conjunction.length > 1) {
-                    output += `<p>${def.partOfSpeech}</p>`
-                }
-                conjunction.forEach(conjunction => {
-                    output += `<li>${conjunction.definition}</li>`
-                })
+            });
+
+            document.querySelector('.output').innerHTML = output;
+            let audio = new Audio(data[0].phonetics[0].audio || data[0].phonetics[1].audio)
+            let sound = document.querySelector('.play');
+            sound.onclick = () => {
+                audio.play()
             }
         })
-    });
-
-    document.querySelector('.output').innerHTML = output;
-    })
-    .catch(err => document.querySelector('.output').innerHTML =
-    `<p>This word, ${search.value}, is not found. Please, check the spelling of the word and try again!</p>`
-    )
-    }
+        .catch(err => document.querySelector('.output').innerHTML =
+            `<p>This word, ${search.value}, is not found. Please, check the spelling of the word and try again!</p>`
+        )
+}
