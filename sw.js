38abd641f9bf022db6ee99b2a16d15a5771ef91e
Savio09 <44920970+Savio09@users.noreply.github.com>
2023-02-13 12:04:22 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-//Cache App Assets and firing install events on service worker
+//Cache App Assets and firing install events on sw
 const staticCache = 'site-static'
 const appShellAssets = [
     "./",
