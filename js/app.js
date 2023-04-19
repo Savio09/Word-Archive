985aaa3d81208e18133dad4555d55863635ecd7e
Savio09 <declanfortune5@gmail.com>
2023-04-19 14:17:49 +0100

    Updates
    Updated my js file to change play icon to pause icon. Modified service worker to reflect new changes

--- js/app.js
+++ js/app.js
@@ -1,141 +1,170 @@
+let search = document.querySelector("#search"),
+  searchBtn = document.querySelector("#search-btn"),
+  font = document.querySelector("select"),
+  body = document.querySelector("body"),
+  toggle = document.querySelector(".toggle");
 
-let search = document.querySelector('#search'),
-    searchBtn = document.querySelector('#search-btn'),
-    font = document.querySelector('select'),
-    body = document.querySelector('body'),
-    toggle = document.querySelector('.toggle');
-
-toggle.addEventListener('click', () => {
-    body.classList.toggle("dark-mode");
-    body.querySelector('.cover').classList.toggle('dark-mode');
-    body.querySelector('a').classList.toggle('dark-mode');
-    font.classList.toggle("dark-mode");
-})
+toggle.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  body.querySelector(".cover").classList.toggle("dark-mode");
+  body.querySelector("a").classList.toggle("dark-mode");
+  font.classList.toggle("dark-mode");
+});
 
 // Select font styles
 font.onchange = () => {
-    body.querySelector(".output").style.fontFamily = font.value;
-}
+  body.querySelector(".output").style.fontFamily = font.value;
+};
 
 // Search Item function
-searchBtn.addEventListener('click', query);
+searchBtn.addEventListener("click", query);
 
 function query(e) {
-    e.preventDefault();
-    if (search.value === "") {
-            document.querySelector('.output').innerHTML = `<h3>Please enter a word into the search box</h3>`
-    }
-    else {
-        // Fetch API Request
-        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${search.value}`)
-            .then(res => res.json())
-            .then(data => {
-                // Fetch transcription
-                let transcription = data[0].phonetic || data[0].phonetics[1].text
+  e.preventDefault();
+  if (search.value === "") {
+    document.querySelector(
+      ".output"
+    ).innerHTML = `<h3>Please enter a word into the search box</h3>`;
+  } else {
+    // Fetch API Request
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${search.value}`)
+      .then((res) => res.json())
+      .then((data) => {
+        // Fetch transcription
+        let transcription = data[0].phonetic || data[0].phonetics[1].text;
 
-                //Modify Output content
-                let output = `
+        //Modify Output content
+        let output = `
                 <div class= "result">
                     <div>
                         <h1 class="title">${search.value}</h1>
                         <span class="transcript">${transcription}</span>
                     </div>
-                    <button class="play" id="play"><i class="fa-solid fa-play"></i></button>
+                    <button class="play" id="play"><i class="fa-solid fa-play" id="state"></i></button>
                 </div>
-                `
-                data.forEach((uniqueRes, index) => {
-                    let meaning = uniqueRes.meanings;
-                    meaning.forEach((def, index) => {
+                `;
+        data.forEach((uniqueRes, index) => {
+          let meaning = uniqueRes.meanings;
+          meaning.forEach((def, index) => {
+            //Pull nouns
+            if (def.partOfSpeech === "noun") {
+              output += `<h3>${def.partOfSpeech}</h3>`;
+              const nouns = [...def.definitions];
+              const nounsList = nouns.map((noun) => noun);
+              const nounMeanings = nounsList.map((x) => x.definition);
+              nounMeanings.forEach(
+                (meaning) =>
+                  (output += `<li class='definition'>${meaning}</li>`)
+              );
+            }
 
-                        //Pull nouns
-                        if (def.partOfSpeech === 'noun') {
-                            output += `<h3>${def.partOfSpeech}</h3>`
-                            const nouns = [...def.definitions]
-                            const nounsList = nouns.map(noun => noun)
-                            const nounMeanings = nounsList.map(x => x.definition)
-                            nounMeanings.forEach(meaning => output +=
-                                `<li class='definition'>${meaning}</li>`)
-                        }
+            // Pull Verbs
+            if (def.partOfSpeech === "verb") {
+              output += `<h3>${def.partOfSpeech}</h3>`;
+              const verbs = [...def.definitions];
+              const verbsList = verbs.map((verb) => verb);
+              const verbMeanings = verbsList.map((x) => x.definition);
+              verbMeanings.forEach(
+                (meaning) =>
+                  (output += `<li class ='definition'>${meaning}</li>`)
+              );
+            }
 
-                        // Pull Verbs
-                        if (def.partOfSpeech === 'verb') {
-                            output += `<h3>${def.partOfSpeech}</h3>`
-                            const verbs = [...def.definitions]
-                            const verbsList = verbs.map(verb => verb)
-                            const verbMeanings = verbsList.map(x => x.definition)
-                            verbMeanings.forEach(meaning => output +=
-                                `<li class ='definition'>${meaning}</li>`)
-                        }
+            //Pull pronouns
+            if (def.partOfSpeech === "pronoun") {
+              output += `<h3>${def.partOfSpeech}</h3>`;
+              const pronouns = [...def.definitions];
+              const pronounsList = pronouns.map((pronoun) => pronoun);
+              const pronounMeanings = pronounsList.map((x) => x.definition);
+              pronounMeanings.forEach(
+                (meaning) =>
+                  (output += `<li class='definition'>${meaning}</li>`)
+              );
+            }
 
-                        //Pull pronouns
-                        if (def.partOfSpeech === 'pronoun') {
-                            output += `<h3>${def.partOfSpeech}</h3>`
-                            const pronouns = [...def.definitions]
-                            const pronounsList = pronouns.map(pronoun => pronoun)
-                            const pronounMeanings = pronounsList.map(x => x.definition)
-                            pronounMeanings.forEach(meaning => output +=
-                                `<li class='definition'>${meaning}</li>`)
-                        }
+            //Pull interjections
+            if (def.partOfSpeech === "exclamation") {
+              output += `<h3>${def.partOfSpeech}</h3>`;
+              const exclamations = [...def.definitions];
+              const exclamationsList = exclamations.map(
+                (exclamation) => exclamation
+              );
+              const exclamationMeanings = exclamationsList.map(
+                (x) => x.definition
+              );
+              exclamationMeanings.forEach(
+                (meaning) =>
+                  (output += `<li class='definition'>${meaning}</li>`)
+              );
+            }
 
-                        //Pull interjections
-                        if (def.partOfSpeech === 'exclamation') {
-                            output += `<h3>${def.partOfSpeech}</h3>`
-                            const exclamations = [...def.definitions]
-                            const exclamationsList = exclamations.map(exclamation => exclamation)
-                            const exclamationMeanings = exclamationsList.map(x => x.definition)
-                            exclamationMeanings.forEach(meaning => output +=
-                                `<li class='definition'>${meaning}</li>`)
-                        }
+            //Pull adjectives
+            if (def.partOfSpeech === "adjective") {
+              output += `<h3>${def.partOfSpeech}</h3>`;
+              const adjectives = [...def.definitions];
+              const adjectivesList = adjectives.map((adjective) => adjective);
+              const adjectiveMeanings = adjectivesList.map((x) => x.definition);
+              adjectiveMeanings.forEach(
+                (meaning) =>
+                  (output += `<li class='definition'>${meaning}</li>`)
+              );
+            }
 
-                        //Pull adjectives
-                        if (def.partOfSpeech === 'adjective') {
-                            output += `<h3>${def.partOfSpeech}</h3>`
-                            const adjectives = [...def.definitions]
-                            const adjectivesList = adjectives.map(adjective => adjective)
-                            const adjectiveMeanings = adjectivesList.map(x => x.definition)
-                            adjectiveMeanings.forEach(meaning => output +=
-                                `<li class='definition'>${meaning}</li>`)
-                        }
+            //Pull adverbs
+            if (def.partOfSpeech === "adverb") {
+              output += `<h3>${def.partOfSpeech}</h3>`;
+              const adverbs = [...def.definitions];
+              const adverbsList = adverbs.map((adverb) => adverb);
+              const adverbMeanings = adverbsList.map((x) => x.definition);
+              adverbMeanings.forEach(
+                (meaning) =>
+                  (output += `<li class='definition'>${meaning}</li>`)
+              );
+            }
 
-                        //Pull adverbs
-                        if (def.partOfSpeech === 'adverb') {
-                            output += `<h3>${def.partOfSpeech}</h3>`
-                            const adverbs = [...def.definitions]
-                            const adverbsList = adverbs.map(adverb => adverb)
-                            const adverbMeanings = adverbsList.map(x => x.definition)
-                            adverbMeanings.forEach(meaning => output +=
-                                `<li class='definition'>${meaning}</li>`)
-                        }
+            //Pull conjuctions
+            if (def.partOfSpeech === "conjunction") {
+              output += `<h3>${def.partOfSpeech}</h3>`;
+              const conjunctions = [...def.definitions];
+              const conjunctionsList = conjunctions.map(
+                (conjunction) => conjunction
+              );
+              const conjunctionMeanings = conjunctionsList.map(
+                (x) => x.definition
+              );
+              verbMeanings.forEach(
+                (meaning) =>
+                  (output += `<li class='definition'>${meaning}</li>`)
+              );
+            }
+          });
+        });
 
-                        //Pull conjuctions
-                        if (def.partOfSpeech === 'conjunction') {
-                            output += `<h3>${def.partOfSpeech}</h3>`
-                            const conjunctions = [...def.definitions]
-                            const conjunctionsList = conjunctions.map(conjunction => conjunction)
-                            const conjunctionMeanings = conjunctionsList.map(x => x.definition)
-                            verbMeanings.forEach(meaning => output +=
-                                `<li class='definition'>${meaning}</li>`)
-                        }
-                    })
-                });
+        //Display information to DOM
+        document.querySelector(".output").innerHTML = output;
 
-                //Display information to DOM
-                document.querySelector('.output').innerHTML = output;
-
-                //Fetch sounds and play if available
-                let audio = new Audio(data[0].phonetics[0].audio || data[0].phonetics[1].audio);
-                let sound = document.querySelector('.play');
-                sound.onclick = () => {
-                    audio.play();
-                }
-            })
-            //Error if no result is found
-            .catch(err => document.querySelector('.output').innerHTML =
-                `<div class="content">
+        //Fetch sounds and play if available
+        let audio = new Audio(
+          data[0].phonetics[0].audio || data[0].phonetics[1].audio
+        );
+        let sound = document.querySelector(".play");
+        let state = document.querySelector("#state");
+        sound.onclick = () => {
+          audio.play();
+          state.classList.replace("fa-play", "fa-pause");
+          audio.onended = function () {
+            state.classList.replace("fa-pause", "fa-play");
+          };
+        };
+      })
+      //Error if no result is found
+      .catch(
+        (err) =>
+          (document.querySelector(".output").innerHTML = `<div class="content">
                 <img src="./svgs/not-found.svg" id="svg">
                 <p>This word, ${search.value}, is not found. Please, check the spelling of the word and try again!</p>
                 </div>
-                `
-            )
-    }
+                `)
+      );
+  }
 }
--- sw.js
+++ sw.js
@@ -1,35 +1,35 @@
 //Cache App Assets and firing install events on service worker
 // Updated icons folder and files
-const staticCache = 'site-static'
+//Updating the js file
+const staticCache = "site-static";
 const appShellAssets = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Lobster&family=Lobster+Two&family=Montserrat:wght@300&family=Raleway:wght@300&family=Roboto:wght@300&family=Source+Serif+Pro:wght@600&display=swap",
-    "https://kit.fontawesome.com/4c0c5e2d66.js"
-]
+  "./",
+  "./index.html",
+  "./style.css",
+  "https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Lobster&family=Lobster+Two&family=Montserrat:wght@300&family=Raleway:wght@300&family=Roboto:wght@300&family=Source+Serif+Pro:wght@600&display=swap",
+  "https://kit.fontawesome.com/4c0c5e2d66.js",
+];
 //install event
-self.addEventListener("install", evt => {
-    console.log('Installation complete');
-    evt.waitUntil(
-        caches.open(staticCache).then(cache => {
-            console.log('Caching app shell items')
-            cache.addAll(appShellAssets);
-        })
-    )
-   
-})
+self.addEventListener("install", (evt) => {
+  console.log("Installation complete");
+  evt.waitUntil(
+    caches.open(staticCache).then((cache) => {
+      console.log("Caching app shell items");
+      cache.addAll(appShellAssets);
+    })
+  );
+});
 //activate events
-self.addEventListener('activate', evt => {
-    console.log('Activation complete')
-})
+self.addEventListener("activate", (evt) => {
+  console.log("Activation complete");
+});
 
 //fetch event
-self.addEventListener("fetch", evt => {
-    //console.log('fetch event', evt);
-    evt.respondWith(
-        caches.match(evt.request).then(cacheRes => {
-            return cacheRes || fetch(evt.request)
-        })
-    )
-})
+self.addEventListener("fetch", (evt) => {
+  //console.log('fetch event', evt);
+  evt.respondWith(
+    caches.match(evt.request).then((cacheRes) => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
